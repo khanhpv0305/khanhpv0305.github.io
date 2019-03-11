efb577293d432a51207c113869d7b7ad8cae811a
Khanh <khanh-van.pham@vn.devintion.com>
2019-03-11 11:38:58 +0700

    Updates

---
+++ precache-manifest.6948a98403ea200ea2307d87a22f3ecd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "637705b518d5685ae50f",
+    "url": "/static/js/main.2125b075.chunk.js"
+  },
+  {
+    "revision": "7dac582a3e19918ef71c",
+    "url": "/static/js/2.fe21df1f.chunk.js"
+  },
+  {
+    "revision": "7dac582a3e19918ef71c",
+    "url": "/static/css/2.810ac56a.chunk.css"
+  },
+  {
+    "revision": "e61312958cd6386c32663984d8713956",
+    "url": "/index.html"
+  }
+];
